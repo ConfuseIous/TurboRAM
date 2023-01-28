72882671e036f0f7afa1b43b6d1e9b05cc046470
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-01-28 21:59:16 +0800

    custom preferences

--- TurboRAM/Views/SettingsView.swift
+++ TurboRAM/Views/SettingsView.swift
@@ -9,21 +9,91 @@ import SwiftUI
 
 struct SettingsView: View {
 	
+	@State private var shouldShowConfirmationAlert: Bool = false
 	@Binding var shouldShowSettingsSheet: Bool
+	
+	@State private var threshold: String = String(UserDefaults.standard.double(forKey: "minimumMemoryUsageThreshold"))
+	@State private var minimumMultiplier: String = String(UserDefaults.standard.double(forKey: "minimumMemoryUsageminimumMultiplier"))
 	
 	var body: some View {
 		VStack {
 			HStack {
 				Text("Settings")
+					.font(.system(size: 30))
 				Spacer()
 				Button(action: {
 					shouldShowSettingsSheet.toggle()
 				}, label: {
-					Text("Done")
+					Text("Cancel")
 				}).padding(.leading)
 			}.padding(.bottom)
 			Divider()
-			Text("TurboRAM will alert you if any of your processes see an increase in memory usage of over 50% from the time they were first tracked and are now using 500MB of memory or more")
+			Text("TurboRAM will alert you if any of process uses \(minimumMultiplier) times more memory from when it was first tracked and is now using \(threshold)MB of memory or more.")
+				.font(.system(size: 12))
+				.foregroundColor(.secondary)
+			VStack {
+				HStack {
+					Text("Ignore processes that use less than:")
+					Spacer()
+				}
+				HStack {
+					TextField("", text: $threshold)
+					Text("(in MB)")
+				}
+				HStack {
+					Text("of memory.")
+					Spacer()
+				}
+			}.padding(.vertical)
+			VStack {
+				HStack {
+					Text("Warn me if a process uses:")
+					Spacer()
+				}
+				TextField("", text: $minimumMultiplier)
+				HStack {
+					Text("times more memory than when it was first tracked.")
+					Spacer()
+				}
+			}.padding(.vertical)
+			Spacer()
+			Button(action: {
+				let acceptableThresholdRange = 200.0...1000.0
+				guard let thresholdDouble = Double(threshold), acceptableThresholdRange.contains(thresholdDouble) else {
+					shouldShowConfirmationAlert.toggle()
+					return
+				}
+				
+				let acceptableMultiplierRange = 1.2...2
+				guard let minimumMultiplierDouble = Double(minimumMultiplier), acceptableMultiplierRange.contains(minimumMultiplierDouble) else {
+					shouldShowConfirmationAlert.toggle()
+					return
+				}
+				
+				UserDefaults.standard.set(thresholdDouble, forKey: "minimumMemoryUsageThreshold")
+				UserDefaults.standard.set(minimumMultiplierDouble, forKey: "minimumMemoryUsageminimumMultiplier")
+				
+				shouldShowSettingsSheet.toggle()
+			}, label: {
+				Image(systemName: "checkmark.circle.fill")
+					.font(.system(size: 40))
+			}).buttonStyle(.borderless)
+			Spacer()
+		}
+		.alert(isPresented: $shouldShowConfirmationAlert) {
+			Alert(
+				title: Text("These values appear to be irregular. Are you sure you want to save them?"),
+				primaryButton: .destructive(Text("Yes")) {
+					if let thresholdDouble = Double(threshold) {
+						UserDefaults.standard.set(thresholdDouble, forKey: "minimumMemoryUsageThreshold")
+					}
+					if let minimumMultiplierDouble = Double(minimumMultiplier) {
+						UserDefaults.standard.set(minimumMultiplierDouble, forKey: "minimumMemoryUsageminimumMultiplier")
+					}
+					
+					shouldShowSettingsSheet.toggle()
+				},
+				secondaryButton: .cancel())
 		}
 		.frame(width: 400, height: 400)
 		.padding()
