98274b0f1a01f835fadedffe77e02cba7a0275fb
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-01-29 21:10:23 +0800

    bug fixes'

--- TurboRAM/Model/MemoryInfoViewModel.swift
+++ TurboRAM/Model/MemoryInfoViewModel.swift
@@ -140,7 +140,10 @@ class MemoryInfoViewModel: ObservableObject {
 	
 	func findOffendingProcesses() {
 		// Returns all processes that grew memory usage by at least 50% since the first run and are using at least 500MB
-		let commonProcesses: [ProcessDetails] = processes.filter({$0.memoryUsage >= UserDefaults.standard.float(forKey: "minimumMemoryUsageThreshold")}).compactMap { process in
+		let commonProcesses: [ProcessDetails] = processes.filter({
+			let proc = $0
+			return (!ignoredProcessIDs.contains(where: {$0 == proc.id}) && $0.memoryUsage >= UserDefaults.standard.float(forKey: "minimumMemoryUsageThreshold"))
+		}).compactMap { process in
 			guard let compared = initialValues[process.id], process.memoryUsage >= (compared * UserDefaults.standard.float(forKey: "minimumMemoryUsageminimumMultiplier")) else { return nil }
 			return process
 		}
